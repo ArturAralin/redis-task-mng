73d6f73b0f7d03f2b265e9a861e886a87ee2a7e8
Artur Aralin <artur.aralin97@gmail.com>
2025-02-24 09:35:31 +0300

    Add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,8 +2,11 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier/recommended';
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.strict,
+  tseslint.configs.stylisticTypeChecked,
+  prettier,
 );
